29e81a1cafa54f374c165d5bd7de615772be91ca
Paulo Cesar Zanco <pzanco@debian>
2017-10-25 13:17:00 -0200

    readme

--- App.jsx
+++ App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    }
+
+    this.setStateHandler = this.setStateHandler.bind(this);
+  };
+
+  setStateHandler() {
+    this.setState({
+      data: [this.state.data, "setState"]
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.setStateHandler}>SET STATE</button>
+        <h4>State Array: {this.state.data}</h4>
+      </div>
+    );
+  }
 }
 
 export default App;
